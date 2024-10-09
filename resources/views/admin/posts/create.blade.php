b35b5eed3ff559c7d7f4b65ecf52905c57b39152
RobyLedda94 <robyledda01@yahoo.it>
2024-10-09 18:29:02 +0200

    [UPD] nel controller recupero i type, definita òa create con la select in cui ciclo i tipi

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -26,6 +26,14 @@
                         <label for="" class="control-label">Immagine</label>
                         <input type="file" name="cover_image" id="cover_image" class="form-control form-control-sm">
                     </div>
+                    <div class="col-12 my-2">
+                        <label for="" class="control-label">Seleziona Tipologia</label>
+                        <select name="type" id="" class="form-select form-select-sm">
+                            @foreach($types as $type)
+                                <option value="{{$type->id}}">{{ $type->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                     <div class="form-group my-3">
                         <label for="content" class="control-label"><strong>Contenuto :</strong></label>
                         <textarea class="form-control form-control-sm w-100 textarea-sm" name="content" id="content-post" placeholder="Inserisci il contenuto" required>{{ old('content') }}</textarea>
